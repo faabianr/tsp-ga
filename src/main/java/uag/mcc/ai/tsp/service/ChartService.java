fc306cf3b8a781639d56d312515b0fc82d753841
Fabian Rivera <fabian.riveram@gmail.com>
2021-06-15 19:23:46 -0500

    update chart styles

--- src/main/java/uag/mcc/ai/tsp/service/ChartService.java
+++ src/main/java/uag/mcc/ai/tsp/service/ChartService.java
@@ -4,7 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
 import org.knowm.xchart.style.Styler;
+import org.knowm.xchart.style.lines.SeriesLines;
+import org.knowm.xchart.style.markers.Marker;
+import org.knowm.xchart.style.markers.SeriesMarkers;
 import uag.mcc.ai.tsp.model.City;
 
 import java.awt.*;
@@ -66,12 +70,13 @@ public class ChartService {
                     .width(1200).height(600).title(BEST_DISTANCE_CHART_TITLE)
                     .xAxisTitle(BEST_DISTANCE_CHART_X_AXIS_TITLE).yAxisTitle(BEST_DISTANCE_CHART_Y_AXIS_TITLE).build();
 
-            bestDistancesChart.getStyler().setMarkerSize(0);
+            bestDistancesChart.getStyler().setMarkerSize(5);
+            bestDistancesChart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNE);
+            bestDistancesChart.getStyler().setSeriesMarkers(new Marker[]{ SeriesMarkers.CIRCLE });
             bestDistancesChart.getStyler().setToolTipsEnabled(true);
+            bestDistancesChart.getStyler().setPlotGridLinesVisible(false);
+            bestDistancesChart.getStyler().setPlotBorderVisible(false);
             bestDistancesChart.getStyler().setToolTipFont(new Font("Verdana", Font.PLAIN, 10));
-            bestDistancesChart.getStyler().setToolTipHighlightColor(Color.BLACK);
-            bestDistancesChart.getStyler().setToolTipBorderColor(Color.DARK_GRAY);
-            bestDistancesChart.getStyler().setToolTipBackgroundColor(Color.LIGHT_GRAY);
             bestDistancesChart.getStyler().setToolTipType(Styler.ToolTipType.xAndYLabels);
 
             bestDistancesChart.addSeries(BEST_DISTANCE_CHART_SERIES_NAME, bestDistancesChartValues.get(X), bestDistancesChartValues.get(Y), null);
@@ -84,12 +89,14 @@ public class ChartService {
 
 
             routeChart.getStyler().setMarkerSize(10);
-            routeChart.getStyler().setSeriesColors(new Color[]{ Color.ORANGE });
+            routeChart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNE);
+            routeChart.getStyler().setSeriesMarkers(new Marker[]{ SeriesMarkers.DIAMOND });
+            routeChart.getStyler().setSeriesLines(new BasicStroke[]{ SeriesLines.SOLID });
+            routeChart.getStyler().setPlotGridLinesVisible(false);
+            routeChart.getStyler().setPlotBorderVisible(false);
+            routeChart.getStyler().setSeriesColors(new Color[]{ Color.RED });
             routeChart.getStyler().setToolTipsEnabled(true);
             routeChart.getStyler().setToolTipFont(new Font("Verdana", Font.PLAIN, 10));
-            routeChart.getStyler().setToolTipHighlightColor(Color.BLACK);
-            routeChart.getStyler().setToolTipBorderColor(Color.DARK_GRAY);
-            routeChart.getStyler().setToolTipBackgroundColor(Color.LIGHT_GRAY);
             routeChart.getStyler().setToolTipsAlwaysVisible(true);
 
             swingWrapper = new SwingWrapper<>(Arrays.asList(routeChart, bestDistancesChart));
